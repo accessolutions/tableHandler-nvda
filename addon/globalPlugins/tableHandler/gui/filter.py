1faa63fc33350174dc2230e8e07e4a2e272042c0
Julien Cochuyt <JulienCochuyt@users.noreply.github.com>
2021-11-30 11:31:48 +0100

    Support toggling marked columns from the context menu

---
+++ addon/globalPlugins/tableHandler/gui/filter.py
@@ -0,0 +1,113 @@
+# globalPlugins/tableHander/gui/filter.py
+# -*- coding: utf-8 -*-
+
+# This file is part of Table Handlerfor NVDA.
+# Copyright (C) 2020-2021 Accessolutions (http://accessolutions.fr)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# See the file COPYING.txt at the root of this distribution for more details.
+
+"""Table Filter GUI."""
+
+# Keep compatible with Python 2
+from __future__ import absolute_import, division, print_function
+
+__version__ = "2021.11.22"
+__author__ = "Julien Cochuyt <j.cochuyt@accessolutions.fr>"
+
+
+import wx
+
+import api
+import addonHandler
+import config
+from core import callLater
+from eventHandler import queueEvent
+import gui
+from logHandler import log
+
+from ..brailleUtils import brailleCellsDecimalStringToUnicode
+from ..nvdaVersion import nvdaVersion
+from ..coreUtils import translate, wx_CallAfter
+
+try:
+	from gui import SettingsDialog, SettingsPanel
+except ImportError:
+	from ..backports.nvda_2018_2.gui_settingsDialogs import SettingsDialog, SettingsPanel
+try:
+	import guiHelper
+except ImportError:
+	from ..backports.nvda_2016_4 import gui_guiHelper as guiHelper
+
+
+addonHandler.initTranslation()
+
+
+@wx_CallAfter
+def show(table, text, caseSensitive):
+	gui.mainFrame.prePopup()
+	FilterDialog(table, text, caseSensitive).ShowModal()
+	gui.mainFrame.postPopup()
+
+
+class FilterDialog(wx.Dialog):
+	
+	def __init__(self, table, text, caseSensitive):
+		# Translators: Table Filter Dialog title
+		super().__init__(gui.mainFrame, title=_("Filter"))
+
+		self.table = table
+
+		mainSizer = wx.BoxSizer(wx.VERTICAL)
+		sHelper = guiHelper.BoxSizerHelper(self, orientation=wx.VERTICAL)
+
+		item = self.text = sHelper.addLabeledControl(
+			# Translators: Table filter Dialog prompt text
+			_("Review only the table rows containing this text:"),
+			wx.TextCtrl
+		)
+		item.Value = text or ""
+		
+		item = self.caseSensitive = sHelper.addItem(
+			# Translators: The label for a settings in the Table Mode settings panel
+			wx.CheckBox(self, label=translate("Case &sensitive"))
+		)
+		item.Value = caseSensitive if caseSensitive is not None else False
+		
+		sHelper.addDialogDismissButtons(self.CreateButtonSizer(wx.OK | wx.CANCEL))
+		mainSizer.Add(sHelper.sizer, border=guiHelper.BORDER_FOR_DIALOGS, flag=wx.ALL)
+		self.Bind(wx.EVT_BUTTON,self.onOk, id=wx.ID_OK)
+		self.Bind(wx.EVT_BUTTON,self.onCancel, id=wx.ID_CANCEL)
+		mainSizer.Fit(self)
+		self.SetSizer(mainSizer)
+		self.CentreOnScreen()
+		self.text.SetFocus()
+
+	def onOk(self, evt):
+		table = self.table
+		text = self.text.Value
+		caseSensitive = self.caseSensitive.Value
+		#table._shouldReportNextFocusEntered = False
+		callLater(
+			100,
+			table._onTableFilterChange,
+			text=text,
+			caseSensitive=caseSensitive
+		)
+		self.Destroy()
+
+	def onCancel(self, evt):
+		#self.table._shouldReportNextFocusEntered = False
+		self.Destroy()
---
+++ addon/globalPlugins/tableHandler/gui/menu.py
@@ -0,0 +1,298 @@
+# globalPlugins/tableHandler/gui/menu.py
+# -*- coding: utf-8 -*-
+
+# This file is part of Table Handler for NVDA.
+# Copyright (C) 2020-2021 Accessolutions (http://accessolutions.fr)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# See the file COPYING.txt at the root of this distribution for more details.
+
+"""Table Handler contextual menu"""
+
+# Keep compatible with Python 2
+from __future__ import absolute_import, division, print_function
+
+__version__ = "2021.11.30"
+__author__ = "Julien Cochuyt <j.cochuyt@accessolutions.fr>"
+
+ 
+import wx
+
+import addonHandler
+addonHandler.initTranslation()
+import controlTypes
+import gui
+import ui
+
+from ..behaviors import Cell, TableManager
+from ..nvdaVersion import nvdaVersion
+from ..scriptUtils import getScriptGestureMenuHint
+from ..tableUtils import getColumnHeaderTextSafe, getRowHeaderTextSafe
+
+def show():
+	gui.mainFrame.prePopup()
+	wx.CallAfter(Menu().Show)
+	gui.mainFrame.postPopup()
+
+
+
+class Menu(wx.Menu):
+	
+	def __init__(self):
+		super(Menu, self).__init__()
+		
+		prevFocus = gui.mainFrame.prevFocus
+		if isinstance(prevFocus, Cell):
+			cell = prevFocus
+			cfg = cell.table._tableConfig
+			rowNum = cell.rowNumber
+			colNum = cell.columnNumber
+			
+			sub = wx.Menu()
+			
+			colHeaRowNum = cfg["columnHeaderRowNumber"]
+			if cell.role == controlTypes.ROLE_TABLECOLUMNHEADER:
+				# Translators: An entry in the context menu Table Mode > Column Headers
+				label = _("Use the default column headers of this table")
+			else:
+				# Translators: An entry in the context menu Table Mode > Column Headers
+				label = _("Use this row as column headers")
+			hint = getScriptGestureMenuHint(TableManager, TableManager.script_setColumnHeader)
+			if hint:
+				label += "\t{}".format(hint)
+			item = sub.AppendCheckItem(wx.ID_ANY, label)
+			self.Bind(wx.EVT_MENU, self.onSetColHeaderRowNumber, item)
+			if colHeaRowNum == rowNum or (
+				colHeaRowNum == None and cell.role == controlTypes.ROLE_TABLECOLUMNHEADER
+			):
+				item.Check()
+			
+			if colNum in cfg["customColumnHeaders"]:
+				# Translators: An entry in the context menu Table Mode > Column Headers
+				label = _("&Customized: {}").format(cfg["customColumnHeaders"][colNum])
+			else:
+				# Translators: An entry in the context menu Table Mode > Column Headers
+				label = _("&Customize the header of this column")
+			item = sub.AppendCheckItem(wx.ID_ANY, label)
+			self.Bind(wx.EVT_MENU, self.onCustomizeColHeader, item)
+			if colNum in cfg["customColumnHeaders"]:
+				item.Check()
+			
+			# Translators: An entry in the Table Mode context menu
+			self.AppendSubMenu(sub, _("Column Headers"))
+			
+			
+			sub = wx.Menu()
+			
+			rowHeaColNum = cfg["rowHeaderColumnNumber"]
+			if cell.role == controlTypes.ROLE_TABLEROWHEADER:
+				# Translators: An entry in the context menu Table Mode > Row Headers
+				label = _("Use the default row headers of this table")
+			else:
+				# Translators: An entry in the context menu Table Mode > Row Headers
+				label = _("Use this column as row headers")
+			hint = getScriptGestureMenuHint(TableManager, TableManager.script_setRowHeader)
+			if hint:
+				label += "\t{}".format(hint)
+			item = sub.AppendCheckItem(wx.ID_ANY, label)
+			self.Bind(wx.EVT_MENU, self.onSetRowHeaderColNumber, item)
+			if rowHeaColNum == colNum or (
+				rowHeaColNum == None and cell.role == controlTypes.ROLE_TABLEROWHEADER
+			):
+				item.Check()
+			
+			if rowNum in cfg["customRowHeaders"]:
+				# Translators: An entry in the context menu Table Mode > Row Headers
+				label = _("&Customized: {}").format(cfg["customRowHeaders"][rowNum])
+			else:
+				# Translators: An entry in the context menu Table Mode > Row Headers
+				label = _("&Customize the header of this row")
+			item = sub.AppendCheckItem(wx.ID_ANY, label)
+			self.Bind(wx.EVT_MENU, self.onCustomizeRowHeader, item)
+			if rowNum in cfg["customRowHeaders"]:
+				item.Check()
+			
+			# Translators: An entry in the Table Mode context menu
+			self.AppendSubMenu(sub, _("Row Headers"))
+			
+			sub = wx.Menu()
+			
+			if rowHeaColNum != colNum:
+				items = {}
+				
+				# Translators: An entry in the context menu Table Mode > Marked Columns
+				items[True] = item = sub.AppendRadioItem(wx.ID_ANY, _("Marked with &announce"))
+				self.Bind(wx.EVT_MENU, self.onToggleMarkedCol_WithAnnounce, item)
+				
+				# Translators: An entry in the context menu Table Mode > Marked Columns
+				items[False] = item = sub.AppendRadioItem(wx.ID_ANY, _("Marked with&out announce"))
+				self.Bind(wx.EVT_MENU, self.onToggleMarkedCol_WithoutAnnounce, item)
+				
+				# Translators: An entry in the context menu Table Mode > Marked Columns
+				items[None] = item = sub.AppendRadioItem(wx.ID_ANY, _("&Not marked"))
+				self.Bind(wx.EVT_MENU, self.onToggleMarkedCol_Unmarked, item)
+				
+				items[cfg["markedColumnNumbers"].get(colNum)].Check()
+			
+			# Translators: An entry in the Table Mode context menu
+			label = _("Marked Columns")
+			hint = getScriptGestureMenuHint(TableManager, TableManager.script_toggleMarkedColumn)
+			if hint:
+				label += "\t{}".format(hint)
+			
+			item = self.AppendSubMenu(sub, label)
+			if rowHeaColNum == colNum:
+				self.Enable(item.Id, False)
+		
+		item = self.Append(
+			wx.ID_ANY,
+			# Translators: An item in NVDA's Preferences menu
+			_("Table Mode Preferences..."),
+			# Translators: The contextual help for an item in NVDA's Preferences menu
+			_("Table Mode Preferences")
+		)
+		self.Bind(wx.EVT_MENU, self.onPreferences, item)
+	
+	def Show(self):
+		gui.mainFrame.prePopup()
+		gui.mainFrame.PopupMenu(self)
+		gui.mainFrame.postPopup()
+	
+	def onSetColHeaderRowNumber(self, evt):
+		cell = gui.mainFrame.prevFocus
+		cfg = cell.table._tableConfig
+		heaNum = cfg["columnHeaderRowNumber"]
+		if cell.role == controlTypes.ROLE_TABLECOLUMNHEADER:
+			if heaNum == False:
+				cfg["columnHeaderRowNumber"] = None
+				headerText = getColumnHeaderTextSafe(cell)
+				# Translators: Announced when customizing column headers
+				ui.message(_("Column header reset to default: {}").format(headerText))
+			else:
+				cfg["columnHeaderRowNumber"] = False
+				# Translators: Announced when customizing column headers
+				ui.message(_("Column header disabled"))
+			return
+		num = cell.rowNumber
+		if heaNum == num:
+			cfg["columnHeaderRowNumber"] = None
+			headerText = getColumnHeaderTextSafe(cell)
+			# Translators: Announced when customizing column headers
+			ui.message(_("Column header reset to default: {}").format(headerText))
+		else:
+			cfg["columnHeaderRowNumber"] = num
+			# Translators: Announced when customizing column headers
+			ui.message(_("Row set as column header"))
+	
+	def onSetRowHeaderColNumber(self, evt):
+		cell = gui.mainFrame.prevFocus
+		cfg = cell.table._tableConfig
+		heaNum = cfg["rowHeaderColumnNumber"]
+		if cell.role == controlTypes.ROLE_TABLEROWHEADER:
+			if heaNum == False:
+				cfg["rowHeaderColumnNumber"] = None
+				headerText = getRowHeaderTextSafe(cell)
+				# Translators: Announced when customizing row headers
+				ui.message(_("Row header reset to default: {}").format(headerText))
+			else:
+				cfg["rowHeaderColumnNumber"] = False
+				# Translators: Announced when customizing row headers
+				ui.message(_("Row header disabled"))
+			return
+		num = cell.columnNumber
+		if heaNum == num:
+			cfg["rowHeaderColumnNumber"] = None
+			headerText = getRowHeaderTextSafe(cell)
+			# Translators: Announced when customizing row headers
+			ui.message(_("Row header reset to default: {}").format(headerText))
+		else:
+			cfg["rowHeaderColumnNumber"] = num
+			# Translators: Announced when customizing row headers
+			ui.message(_("Column set as row header"))
+	
+	def onCustomizeColHeader(self, evt):
+		cell = gui.mainFrame.prevFocus
+		cfg = cell.table._tableConfig
+		customHeaders = cfg["customColumnHeaders"].copy()
+		num = cell.columnNumber
+		if num in customHeaders:
+			del customHeaders[num]
+			cfg["customColumnHeaders"] = customHeaders
+			return
+		dlg = wx.TextEntryDialog(
+			gui.mainFrame,
+			# Translators: A prompt to enter a value
+			message=_("Enter a custom header for this column")
+		)
+		from logHandler import log
+		if dlg.ShowModal() == wx.ID_OK:
+			customHeaders[num] = dlg.Value
+			cfg["customColumnHeaders"] = customHeaders
+			log.info(f"new cfg: {cfg.data}")
+			cfg.save()
+		else:
+			log.info(f"unchanged")
+	
+	def onCustomizeRowHeader(self, evt):
+		cell = gui.mainFrame.prevFocus
+		cfg = cell.table._tableConfig
+		customHeaders = cfg["customRowHeaders"].copy()
+		num = cell.rowNumber
+		if num in customHeaders:
+			del customHeaders[num]
+			cfg["customRowHeaders"] = customHeaders
+			return
+		dlg = wx.TextEntryDialog(
+			gui.mainFrame,
+			# Translators: A prompt to enter a value
+			message=_("Enter a custom header for this row")
+		)
+		if dlg.ShowModal() == wx.ID_OK:
+			customHeaders[num] = dlg.Value
+			cfg["customRowHeaders"] = customHeaders
+			cfg.save()
+	
+	def onToggleMarkedCol_WithAnnounce(self, evt):
+		cell = gui.mainFrame.prevFocus
+		cfg = cell.table._tableConfig
+		marked = cfg["markedColumnNumbers"].copy()
+		num = cell.columnNumber
+		marked[num] = True
+		cfg["markedColumnNumbers"] = marked
+	
+	def onToggleMarkedCol_WithoutAnnounce(self, evt):
+		cell = gui.mainFrame.prevFocus
+		cfg = cell.table._tableConfig
+		marked = cfg["markedColumnNumbers"].copy()
+		num = cell.columnNumber
+		marked[num] = False
+		cfg["markedColumnNumbers"] = marked
+	
+	def onToggleMarkedCol_Unmarked(self, evt):
+		cell = gui.mainFrame.prevFocus
+		cfg = cell.table._tableConfig
+		marked = cfg["markedColumnNumbers"].copy()
+		num = cell.columnNumber
+		marked.pop(num, None)
+		cfg["markedColumnNumbers"] = marked
+	
+	def onPreferences(self, evt):
+		if nvdaVersion >= (2018, 2):
+			from gui.settingsDialogs import NVDASettingsDialog 
+			from .settings import TableHandlerSettingsPanel
+			gui.mainFrame._popupSettingsDialog(NVDASettingsDialog, TableHandlerSettingsPanel)
+		else:
+			from .settings import TableHandlerSettingsDialog
+			gui.mainFrame._popupSettingsDialog(TableHandlerSettingsDialog)
