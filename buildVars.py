3ca2037d2edaf34f04f768c9faac1ae9fdd61814
Julien Cochuyt <JulienCochuyt@users.noreply.github.com>
2025-01-23 08:16:56 +0100

    Bump version to 2025.01.22-dev

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	"addon_description": _("""Description for the add-on.
 It can span multiple lines."""),
 	# version
-	"addon_version": "2025.01.07-dev",
+	"addon_version": "2025.01.22-dev",
 	# Author(s)
 	"addon_author": u"Accessolutions (https://accessolutions.fr)",
 	# URL for the add-on documentation support
