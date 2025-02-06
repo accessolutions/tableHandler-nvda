beab1e0de92dce0384f9d4d446a168253514ab7e
André-Abush Clause <a.clause@accessolutions.fr>
2025-02-06 19:50:54 +0100

    Add DEBUG_MODE flag for conditional error logging

--- addon/globalPlugins/tableHandler/documents.py
+++ addon/globalPlugins/tableHandler/documents.py
@@ -72,6 +72,7 @@ from .textInfoUtils import getField
 addonHandler.initTranslation()
 
 
+DEBUG_MODE = False
 SCRIPT_CATEGORY = "TableHandler"
 
 
@@ -167,11 +168,11 @@ class DocumentTableHandler(TableHandler):
 			colNum = kwargs.get("colNum")
 			if rowNum is not None:
 				table._currentRowNumber = rowNum
-			else:
+			elif DEBUG_MODE:
 				log.error("setPosition: Unspecified row number")
 			if colNum is not None:
 				table._currentColumnNumber = colNum
-			else:
+			elif DEBUG_MODE:
 				log.error("setPosition: Unspecified column number")
 			table._currentRowNumber = rowNum
 			table._currentColumnNumber = colNum
@@ -549,11 +550,11 @@ class TableHandlerBmdti(BrowseModeDocumentTreeInterceptor, DocumentTableHandler)
 				colNum = kwargs.get("colNum")
 				if rowNum is not None:
 					table._currentRowNumber = rowNum
-				else:
+				elif DEBUG_MODE:
 					log.error("setPosition: Unspecified row number")
 				if colNum is not None:
 					table._currentColumnNumber = colNum
-				else:
+				elif DEBUG_MODE:
 					log.error("setPosition: Unspecified column number")
 			if kwargs.get("debug"):
 				log.info(f"<<< THTI.getTableManager: {table} (from super)")
